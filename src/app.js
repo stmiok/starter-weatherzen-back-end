12487d921f73f5d2df8f949af7d184997ef1e2bd
stmiok <128644466+stmiok@users.noreply.github.com>
2023-11-03 15:48:35 -0700

    added files and code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 
+
+
+
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+
+const observationsRouter = require('./observations/observations.router')
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
+/*
+let nextId = 1;
+app.post('/observations', ((req, res) => {
+    const newObservation = req.body.data;
+    
+    newObservation.observation_id = nextId++
+    
+    res.status(201).json({
+        data: newObservation,
+    });
+}))
+*/
+
+app.use("./observations", observationsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
